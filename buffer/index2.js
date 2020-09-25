7b7c1e1a714b0bcce43adfcc4afc442e86d20ec3
kuro <kisuvitinh@gmail.com>
2020-09-25 14:07:59 +0700

    async-hooks, buffer

--- buffer/index2.js
+++ buffer/index2.js
@@ -1,4 +1,19 @@
-const encode = Buffer.from('a', 'ascii')
-console.log(encode)
-const decode = encode.toString('base64')
-console.log(decode)
+// 1
+// const buf = Buffer.from('hello', 'utf16le');
+// const uint16arr = new Uint16Array(
+//   buf.buffer,
+//   buf.byteOffset,
+//   buf.length / Uint16Array.BYTES_PER_ELEMENT
+// );
+// console.log(uint16arr);
+
+
+// 2
+const buf = Buffer.from([1, 2, 3, 4]);
+const uint32array = new Uint32Array(buf);
+
+console.log(uint32array);
+console.log(Uint32Array.BYTES_PER_ELEMENT);
+
+
+
